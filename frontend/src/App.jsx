51850b7f7af2983791c959da1e6e2c3835ede999
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2024-02-13 16:09:35 -0300

    pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,43 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from '../src/screens/About'
+import Dashboard from '../src/screens/Dashboard'
+import Home from '../src/screens/Home'
+import Projects from '../src/screens/Projects'
+import SignIn from '../src/screens/SignIn'
+import SignUp from '../src/screens/SignUp'
+
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						path='/'
+						element={<Home />}
+					/>
+					<Route
+						path='/about'
+						element={<About />}
+					/>
+					<Route
+						path='/sign-in'
+						element={<SignIn />}
+					/>
+					<Route
+						path='/sign-up'
+						element={<SignUp />}
+					/>
+					<Route
+						path='/dashboard'
+						element={<Dashboard />}
+					/>
+					<Route
+						path='/projects'
+						element={<Projects />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</>
+	)
 }
 export default App
