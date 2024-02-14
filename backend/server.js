a81e0204dfaed89c814bf222bbfce35904c49e7a
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2024-02-14 16:43:51 -0300

    DB connection done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+import connectDB from './config/dbConn.js'
 
 const app = express()
-const PORT = 3050
+const PORT = process.env.PORT
 
+connectDB()
 app.listen(PORT, (req, res) => {
-    console.log(`Server running on port ${PORT}\nhttp://localhost:${PORT}`)
+	console.log(`Server running on port ${PORT}\nhttp://localhost:${PORT}`)
+	mongoose.connection.once('open', () => {
+		console.log('Connected to MongoDB')
+	})
 })
