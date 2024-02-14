44039a557df73b09f8a899026f486df7c6d57c57
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2024-02-14 19:35:57 -0300

    testing route and controller

---
+++ backend/controller/userController.js
@@ -0,0 +1,5 @@
+const test = (req, res) => {
+	res.json({message: 'backend working'})
+}
+
+export default test
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ import express from 'express'
 import mongoose from 'mongoose'
 import 'dotenv/config'
 import connectDB from './config/dbConn.js'
+import userRoute from './routes/userRoute.js'
 
 const app = express()
 const PORT = process.env.PORT
 
 connectDB()
+
+app.use('/api/user',userRoute)
+
 app.listen(PORT, (req, res) => {
 	console.log(`Server running on port ${PORT}\nhttp://localhost:${PORT}`)
 	mongoose.connection.once('open', () => {
