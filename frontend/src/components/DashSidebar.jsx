71f0973e7ad0ec746722a7add83392d26b360bfc
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2024-02-21 08:59:59 -0300

    Sidebar dashboard done

---
+++ frontend/src/components/DashSidebar.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { Sidebar } from 'flowbite-react'
+import { sidebarThemeConfig } from '../configs/theme'
+import { HiArrowSmLeft, HiUser } from 'react-icons/hi'
+
+const DashSidebar = () => {
+	const location = useLocation()
+	const [tab, setTab] = useState('')
+	useEffect(() => {
+		const urlParams = new URLSearchParams(location.search)
+		const tabFromUrl = urlParams.get('tab')
+		if (tabFromUrl) {
+			setTab(tabFromUrl)
+		}
+	}, [location.search])
+	return (
+		<Sidebar
+			theme={sidebarThemeConfig}
+			className='w-full md:w-56 '
+		>
+			<Sidebar.Items>
+				<Sidebar.ItemGroup>
+					<Link to='/dashboard?tab=profile'>
+						<Sidebar.Item
+							active={tab === 'profile'}
+							icon={HiUser}
+							label={'User'}
+							labelColor='dark'
+						>
+							Profile
+						</Sidebar.Item>
+					</Link>
+				</Sidebar.ItemGroup>
+				<Sidebar.ItemGroup>
+					<Sidebar.Item
+						icon={HiArrowSmLeft}
+						className='cursor-pointer'
+					>
+						Deslogar
+					</Sidebar.Item>
+				</Sidebar.ItemGroup>
+			</Sidebar.Items>
+		</Sidebar>
+	)
+}
+
+export default DashSidebar
--- frontend/src/configs/theme.js
+++ frontend/src/configs/theme.js
@@ -40,10 +40,90 @@ const navbarThemeConfig = {
 	},
 }
 
-
 const footerThemeConfig = {
 	root: {
 		base: 'w-full rounded-lg bg-bg2_lm shadow border-orange1_lm dark:border-orange1_dm  dark:bg-bg2_dm md:flex md:items-center md:justify-between',
+	},
+}
+
+const sidebarThemeConfig = {
+	root: {
+		base: 'h-full',
+		collapsed: {
+			on: 'w-16',
+			off: 'w-64',
+		},
+		inner:
+			'h-full overflow-y-auto overflow-x-hidden rounded bg-bg2_lm py-4 px-3 dark:bg-bg2_dm',
+	},
+	collapse: {
+		button:
+			'group flex w-full items-center rounded-lg p-2 text-base font-normal text-gray-900 transition duration-75 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700',
+		icon: {
+			base: 'h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white',
+			open: {
+				off: '',
+				on: 'text-gray-900',
+			},
+		},
+		label: {
+			base: 'ml-3 flex-1 whitespace-nowrap text-left',
+			icon: {
+				base: 'h-6 w-6 transition ease-in-out delay-0',
+				open: {
+					on: 'rotate-180',
+					off: '',
+				},
+			},
+		},
+		list: 'space-y-2 py-2',
+	},
+	cta: {
+		base: 'mt-6 rounded-lg p-4 bg-gray-100 dark:bg-gray-700',
+		color: {
+			blue: 'bg-cyan-50 dark:bg-cyan-900',
+			dark: 'bg-dark-50 dark:bg-dark-900',
+			failure: 'bg-red-50 dark:bg-red-900',
+			gray: 'bg-alternative-50 dark:bg-alternative-900',
+			green: 'bg-green-50 dark:bg-green-900',
+			light: 'bg-light-50 dark:bg-light-900',
+			red: 'bg-red-50 dark:bg-red-900',
+			purple: 'bg-purple-50 dark:bg-purple-900',
+			success: 'bg-green-50 dark:bg-green-900',
+			yellow: 'bg-yellow-50 dark:bg-yellow-900',
+			warning: 'bg-yellow-50 dark:bg-yellow-900',
+		},
+	},
+	item: {
+		base: 'flex items-center justify-center rounded-lg p-2 text-base font-normal text-fg0_lm hover:bg-bg0_h_lm dark:text-fg0_dm dark:hover:bg-bg0_h_dm',
+		active: 'bg-bg0_h_lm dark:bg-bg0_h_dm',
+		collapsed: {
+			insideCollapse: 'group w-full pl-8 transition duration-75',
+			noIcon: 'font-bold',
+		},
+		content: {
+			base: 'px-3 flex-1 whitespace-nowrap',
+		},
+		icon: {
+			base: 'h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white',
+			active: 'text-gray-700 dark:text-gray-100',
+		},
+		label: '',
+		listItem: '',
+	},
+	items: {
+		base: '',
+	},
+	itemGroup: {
+		base: 'mt-4 space-y-2 border-t border-orange1_lm pt-4 first:mt-0 first:border-t-0 first:pt-0 dark:border-orange1_dm',
+	},
+	logo: {
+		base: 'mb-5 flex items-center pl-2.5',
+		collapsed: {
+			on: 'hidden',
+			off: 'self-center whitespace-nowrap text-xl font-semibold dark:text-white',
+		},
+		img: 'mr-3 h-6 sm:h-7',
 	},
 }
 
@@ -52,4 +132,5 @@ export {
 	buttonThemeConfig,
 	navbarThemeConfig,
 	footerThemeConfig,
+	sidebarThemeConfig
 }
