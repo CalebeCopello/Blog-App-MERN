b781da0065ad89200ee8669b4b967cec7413901d
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2024-02-23 23:30:23 -0300

    admin user cookie Done

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -53,6 +53,7 @@ const signin = async (req, res, next) => {
 		const token = jwt.sign(
 			{
 				id: validUser._id,
+				isAdmin: validUser.isAdmin,
 			},
 			process.env.JWT_SECRET,
 			{ expiresIn: '30d' }
@@ -69,7 +70,10 @@ const google = async (req, res, next) => {
 	try {
 		const user = await User.findOne({ email })
 		if (user) {
-			const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+			const token = jwt.sign(
+				{ id: user._id, isAdmin: user.isAdmin },
+				process.env.JWT_SECRET
+			)
 			const { password, ...rest } = user._doc
 			res
 				.status(200)
@@ -91,7 +95,10 @@ const google = async (req, res, next) => {
 				profilePicture: googlePhotoURL,
 			})
 			await newUser.save()
-			const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET)
+			const token = jwt.sign(
+				{ id: newUser._id, isAdmin: newUser.isAdmin },
+				process.env.JWT_SECRET
+			)
 			const { password, ...rest } = newUser._doc
 			res
 				.status(200)
