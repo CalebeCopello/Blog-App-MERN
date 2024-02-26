7d5d057054b81e554964ef969131c64707ef2a66
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2024-02-26 18:10:17 -0300

    get posts route done

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import verifyToken from '../utils/verifyToken.js'
-import { create } from '../controller/postController.js'
+import { create, getPosts } from '../controller/postController.js'
 
 const router = express.Router()
 
 
 router.post('/create', verifyToken, create)
+router.get('/getposts', getPosts)
 
 export default router
