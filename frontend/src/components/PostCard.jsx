c9be07412c4cfb2ba11b634f09a118c48006f4a9
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2024-03-04 17:41:05 -0300

    Update PostCard.jsx

--- frontend/src/components/PostCard.jsx
+++ frontend/src/components/PostCard.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function PostCard({ post }) {
 	return (
-		<div className='group relative w-full bg-bg0_h_lm dark:bg-bg0_h_dm border border-orange1_lm dark:border-orange1_dm hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[430px] transition-all'>
+		<div className='group relative w-full bg-bg0_h_lm dark:bg-bg0_h_dm border border-orange1_lm dark:border-orange1_dm hover:border-2 h-[400px] overflow-hidden rounded-lg sm:w-[430px] transition-all shadow-md'>
 			<Link to={`/post/${post.slug}`}>
 				<img
 					src={post.image}
@@ -13,7 +13,7 @@ function PostCard({ post }) {
 			</Link>
 			<div className='p-3 flex flex-col gap-2'>
 				<p className='text-lg font-semibold line-clamp-2'>{post.title}</p>
-				<span className='italic text-sm'>{post.category}</span>
+				<span className='italic text-sm border-2 border-orange0 w-fit mx-auto rounded-xl px-2'>{post.category}</span>
 				<Link
 					to={`/post/${post.slug}`}
 					className='z-10 group-hover:bottom-0 absolute bottom-[-200px] left-0 right-0 border  border-orange1_lm dark:border-orange1_dm bg-bg1_lm dark:bg-bg1_dm hover:bg-orange0 dark:hover:bg-orange0 hover:text-bg1_lm transition-all duration-300 text-center py-2 rounded-md m-2'
